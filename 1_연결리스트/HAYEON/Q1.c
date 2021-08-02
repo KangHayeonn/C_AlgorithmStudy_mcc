e248d73d4afb346b9d2b0b0726b4387eae781ac6
KangHayeonn <78534650+KangHayeonn@users.noreply.github.com>
2021-08-02 21:48:54 +0900

    HAYEON : 최소, 최대
    실패 대실패, 자료형 맞춰줘야지이이ㅣㅇ

--- 1_연결리스트/HAYEON/Q1.c
+++ 1_연결리스트/HAYEON/Q1.c
@@ -1,1 +1,100 @@
-// 작성전
+// 자료구조 1주차
+// 최소, 최대 10818번 ( 백준 )
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* strcpy_hy(char* str1, const char* str2);
+
+// 연결리스트의 node 구조 (data+link)
+typedef struct Node { //typedef를 쓰면 struct 키워드 안 붙이고 별칭 쓸 수 있음.
+    char data[10];
+    struct Node* link; // 다음 노드를 가리키는 포인터
+}listNode;
+
+// 리스트의 시작 (head 노드)
+typedef struct Start_Linked {
+    struct Node* head; // 리스트의 시작 (Node의 주솟값: head)
+}linkedlist_h;
+
+
+// 공백 리스트 생성
+linkedlist_h* createLL(void) {
+    struct Start_Linked* grade; // Node의 첫 주솟값인 head를 값으로 가지고 있는 L
+    grade = (linkedlist_h*)malloc(sizeof(linkedlist_h));
+    grade->head = '\0'; // 리스트가 없다
+    return grade;
+}
+
+// 맨 앞에 노드 삽입
+void unshiftLL(linkedlist_h* grade, char* input) {
+    listNode* newnode;
+    newnode = (listNode*)malloc(sizeof(listNode));
+    strcpy_hy(newnode->data, input);
+    newnode->link = grade->head; // newnode가 맨 앞으로 이동
+    grade->head = newnode; // newnode 의 주솟값 가리킴
+}
+
+int main() {
+    linkedlist_h* grade;
+    int count;
+    int s1[10];
+
+    scanf("%d", &count);
+    int* arr = (int*)malloc(sizeof(int) * count); // 배열 동적할당
+
+    grade = createLL();
+
+
+    for (int i = 0; i < count; i++) {
+        scanf("%d", &arr[i]);
+        sprintf(s1, "%d", arr[i]);
+        unshiftLL(grade, s1);
+
+    }
+    //grade = createLL();
+
+    /*
+    grade = createLL();
+    for (int i = 0; i < count; i++) {
+       printf("arr[%d] : %d ", i, arr[i]);
+    }*/
+ 
+    int before = 0;
+    int after = 0;
+    int max = 0;
+    int min = atoi(grade->head->data);
+    int number = 0;
+
+    while (grade->head != NULL) {
+        number = atoi(grade->head->data);
+        //printf("k: %d\n", k);
+     
+        if (max < number) max = number;
+        else if (min > number) min = number; 
+        grade->head = grade->head->link;
+    }
+    //printf("\n");
+    //printf("최대: %d\n", max);
+    //printf("최소: %d", min);
+    printf("%d %d", min, max);
+
+    //printf("%f",float(6 / 8));
+
+    free(arr); // 메모리 반납
+    return 0;
+}
+
+char* strcpy_hy(char* str1, const char* str2) {
+    int i = 0;
+    while (str2[i] != '\0') {
+        str1[i] = str2[i];
+        i++;
+    }
+    str1[i] = '\0';
+    return str1;
+}
