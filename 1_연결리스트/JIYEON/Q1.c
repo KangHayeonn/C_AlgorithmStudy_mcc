d8d72da0248f2ead5a0230672f5cfe00628b8a34
Sonjiyeon <74100948+Sonjiyeon@users.noreply.github.com>
2021-08-02 21:44:19 +0900

    Update Q1.c

--- 1_연결리스트/JIYEON/Q1.c
+++ 1_연결리스트/JIYEON/Q1.c
@@ -1,1 +1,41 @@
-// 작성전
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int n, i, max= -1000000, min=1000000;
+	int* m;
+	scanf_s("%d\n", &n);
+
+	m = (int*)malloc(sizeof(int) * n);
+
+	//for (int i = 0; i < n; i++) {
+
+	//	scanf_s("%d ", &m[i]);// gets 함수처럼 한번에 읽어들여 사용하고 싶었음.
+
+	//}
+
+	//free(m);
+
+	//max = m[0];
+	//min = m[0];//
+
+	for (int i = 0; i < n; i++) {
+
+	//	scanf_s("%d", &m[i]); gets 함수처럼 한번에 읽어들여 사용하고 싶었음.
+		if (max < m[i]) {
+			max = m[i];
+		}
+		if (min > m[i]) {
+			min = m[i];
+		}
+
+	}
+
+	printf("%d %d", min, max);
+
+	return 0;
+
+
+}
