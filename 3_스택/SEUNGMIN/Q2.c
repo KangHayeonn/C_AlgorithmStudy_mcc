9f572d88ea0f5a05421533dc0e11d888a683d0fd
SoNgSeUNgMiN96 <87978544+SoNgSeUNgMiN96@users.noreply.github.com>
2021-08-17 01:54:10 +0900

    Update Q2.c
    스택을 이용한 백준 "괄호"문제 풀이

--- 3_스택/SEUNGMIN/Q2.c
+++ 3_스택/SEUNGMIN/Q2.c
@@ -1,1 +1,48 @@
-// 작성전
+#include "stack.h"
+
+
+int main(){
+    int N;
+    char temp[51];
+    top * T;
+    stack *cursor;
+    char *temp2;
+    scanf("%d",&N);
+    int status;
+    T = createstack();
+    
+
+    for(int i=0;i<N;i++){
+        status =0;
+        scanf("%s",temp);
+        temp2 = temp;
+        while (*temp2)
+        {
+            if(temp2[0]=='('){
+                push(T,'(');
+            }
+            else{
+                if(T->top!=NULL){ //비지 않았다면
+                    pop(T);
+                }  
+                else{       //비었으면
+                    status=1;
+                    break;
+                }
+            }
+            temp2++;
+        }
+        if(T->top!=NULL){ //다 돌리고나서 비지 않았다면
+            status = 1;
+        }
+        if(status==1){
+            printf("NO\n");
+        }else{
+            printf("YES\n");
+        }
+        while (T->top!=NULL)    //스택을 다 비워준다.
+        {
+            pop(T);
+        }
+    }
+}
