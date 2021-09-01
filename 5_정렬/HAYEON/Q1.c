f9ae6e25dbb64c67470434733382a0cf709b20ff
HAYEON KANG <78534650+KangHayeonn@users.noreply.github.com>
2021-09-01 16:58:53 +0900

    HAYEON : 소트인사이드
    성공
    
    * 버블 정렬 사용
    
    - 작성자 : 강하연
    - 작성일 : 2021-09-01

--- 5_정렬/HAYEON/Q1.c
+++ 5_정렬/HAYEON/Q1.c
@@ -1,1 +1,24 @@
-// 작성전
+// 소트인사이드 (백준 1427번)
+
+#include <stdio.h>
+#include <string.h>
+
+BubbleSort(char list[], int size) {
+	char temp;
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - 1; j++) {
+			if (list[j] > list[j + 1]) {
+				temp = list[j];
+				list[j] = list[j + 1];
+				list[j + 1] = temp;
+			}
+		}
+	}
+}
+int main() {
+	char list[10000];
+	scanf("%s", &list);
+	BubbleSort(list, strlen(list));
+	for (int t = strlen(list) - 1; t >= 0;  t--) printf("%c", list[t]);
+	return 0;
+}
