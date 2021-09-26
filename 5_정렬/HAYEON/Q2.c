6a79e38e2602c71734a5996e9f0bf7b205dc0700
HAYEON KANG <78534650+KangHayeonn@users.noreply.github.com>
2021-09-27 01:01:06 +0900

    퀵정렬 - 좌표정렬하기 (백준 11650번)
    : 성공
    
    * 이차원 배열 이용 (동적 할당)
    * 알고리즘을 매우 단순하게 수정
    * j++는전수탐색으로 매번 더해주고, 조건에 따라 i를 더해준 뒤 i와 j가
      다를 경우에만 Swap
    * 조건은 j가 피봇보다 작을경우!
    
    - 작성자 : 강하연
    - 작성일 : 2021-09-27

--- 5_정렬/HAYEON/Q2.c
+++ 5_정렬/HAYEON/Q2.c
@@ -3,60 +3,243 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int partition(int** list, int begin, int end);
-void QuickSort(int** list, int begin, int end) {
-	if (begin <= end) {
-		int p;
-		p = partition(list, begin, end); // 확정된 피봇
-		QuickSort(list, begin, p-1); // 왼쪽 부분집합에 대해 퀵정렬
-		QuickSort(list, p + 1, end); // 오른쪽 부분집합에 대해 퀵정렬
-	}
+// ver1 : 동적 메모리 할당 ( 이차원 배열 이용 )
+// 실패
+
+/*
+void QuickSort(int **n, int left, int right);
+int Partition(int **n, int left, int right);
+void Swap(int **n, int left, int right);
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int** arr = (int**)malloc(sizeof(int*) * N);
+
+    for (int i = 0; i < N; i++) {
+        arr[i] = (int*)malloc(sizeof(int) * 2);
+    }
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &arr[i][0], &arr[i][1]);
+    }
+
+    QuickSort(arr, 0, N-1);
+
+    for (int i = 0; i < N; i++) {
+        printf("%d %d\n", arr[i][0], arr[i][1]);
+    }
+    return 0;
 }
 
-
-int partition(int** list, int L, int R) {
-	int pivot = (L + R) / 2;
-	int *temp;
-	while (L < R) {
-		/*for (int i = L; i < R; i++) {
-		printf("%p -> %d,%d\n", list[i], list[i][0], list[i][1]);
-		}*/
-		while (list[L][0] < list[pivot][0] && L != R) L++;
-		while (list[R][0] > list[pivot][0] && L != R) R--;
-		while (list[L][0] == list[pivot][0] && list[L][1] < list[pivot][1] && L != R) L++;
-		while (list[R][0] == list[pivot][0] && list[R][1] >= list[pivot][1] && L != R) R--;
-		if (L < R) {
-			temp = list[L];
-			list[L] = list[R];
-			list[R] = temp;
-		}
-		else break;
-	}
-	// L==R 인 경우
-	temp = list[pivot];
-	list[pivot] = list[R];
-	list[R] = temp;
-	return R;
+void QuickSort(int **n, int left, int right) {
+    if (left <= right) {
+        int pivot = Partition(n, left, right);
+        QuickSort(n, left, pivot - 1);
+        QuickSort(n, pivot + 1, right);
+    }
 }
-int main() {
-	int N;
-	scanf("%d", &N);
-	int** list;
-	list = (int**)malloc(sizeof(int*) * N);
-	//list = (int**)calloc(N, sizeof(int*));
-	for (int i = 0; i < N; i++) {
-		list[i] = (int*)malloc(sizeof(int) * 2);
-	}
-	for (int i = 0; i < N; i++) {
-		scanf("%d %d", &list[i][0], &list[i][1]);
-	}
 
-	QuickSort(list, 0, N - 1);
-	for (int i = 0; i < N; i++) {
-		printf("%d %d\n", list[i][0], list[i][1]);
-	}
+int Partition(int **n, int i, int right) {
+    int pivot = n[i][0];
+    int pivot_y = n[i][1];
+    int pivot_idx = i;
+    int j = i + 1;
+    while (j <= right) {
+        if (pivot < n[j][0]) {
+            j++;
+        }
+        else if (pivot == n[j][0]) {
+            if (pivot_y < n[j][1]) {
+                j++;
+            }
+            else {
+                do {
+                    i++;
+                } while (n[i][1] < pivot_y && i < j);
 
-	for (int i = 0; i < N; i++) free(list[i]);
-	free(list);
-	return 0;
+                if (i == j && n[i][1] < pivot_y) {
+                    j++;
+                    continue;
+                }
+                if (i == j && i >= right) break;
+                Swap(n, i, j);
+            }
+        }
+        else {
+            do {
+                i++;
+            } while (n[i][0] < pivot && i < j);
+
+            if (i == j && n[i][0] < pivot) {
+                j++;
+                continue;
+            }
+            if (i == j && i >= right) break;
+            Swap(n, i, j);
+        }
+    }
+    Swap(n, pivot_idx, i);
+    return i;
+}
+
+void Swap(int **n, int left, int right) {
+    int* temp;
+    temp = n[left];
+    n[left] = n[right];
+    n[right] = temp;
+}*/
+
+// ver2 : 구조체 이용
+// 실패
+/*
+typedef struct coordinate {
+    int x;
+    int y;
+}coor;
+
+void QuickSort(coor n[], int left, int right);
+int Partition(coor n[], int left, int right);
+void Swap(coor n[], int left, int right);
+
+int main()
+{
+    int N;
+    coor dot[100000];
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &dot[i].x, &dot[i].y);
+    }
+
+    QuickSort(dot, 0, N - 1);
+
+    for (int i = 0; i < N; i++) {
+        printf("%d %d\n", dot[i].x, dot[i].y);
+    }
+    return 0;
+}
+
+void QuickSort(coor n[], int left, int right) {
+    if (left <= right) {
+        int pivot = Partition(n, left, right);
+        QuickSort(n, left, pivot - 1);
+        QuickSort(n, pivot + 1, right);
+    }
+}
+
+int Partition(coor n[], int i, int right) {
+    int pivot = n[i].x;
+    int pivot_y = n[i].y;
+    int pivot_idx = i;
+    int j = i + 1;
+    while (j <= right) {
+        if (pivot < n[j].x) {
+            j++;
+        }
+        else if (pivot == n[j].x) {
+            if (pivot_y < n[j].y) {
+                j++;
+            }
+            else {
+                do {
+                    i++;
+                } while (n[i].y < pivot_y && i < j);
+
+                if (i == j && n[i].y < pivot_y) {
+                    j++;
+                    continue;
+                }
+                if (i == j && i >= right) break;
+                Swap(n, i, j);
+            }
+        }
+        else {
+            do {
+                i++;
+            } while (n[i].x < pivot && i < j);
+
+            if (i == j && n[i].x < pivot) {
+                j++;
+                continue;
+            }
+            if (i == j && i >= right) break;
+            Swap(n, i, j);
+        }
+    }
+    Swap(n, pivot_idx, i);
+    return i;
+}
+
+void Swap(coor n[], int left, int right) {
+    coor temp;
+    temp = n[left];
+    n[left] = n[right];
+    n[right] = temp;
+}*/
+
+// ver3 : Partition if-else문 수정, 동적 메모리 할당 ( 이차원 배열 이용 )
+// 성공
+
+void QuickSort(int** n, int left, int right);
+int Partition(int** n, int i, int right);
+void Swap(int** n, int left, int right);
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int** arr = (int**)malloc(sizeof(int*) * N);
+
+    for (int i = 0; i < N; i++) {
+        arr[i] = (int*)malloc(sizeof(int) * 2);
+    }
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &arr[i][0], &arr[i][1]);
+    }
+
+    QuickSort(arr, 0, N - 1);
+
+    for (int i = 0; i < N; i++) {
+        printf("%d %d\n", arr[i][0], arr[i][1]);
+    }
+    return 0;
+}
+
+void QuickSort(int** n, int left, int right) {
+    if (left < right) {
+        int pivot = Partition(n, left, right);
+        QuickSort(n, left, pivot - 1);
+        QuickSort(n, pivot + 1, right);
+    }
+}
+
+int Partition(int** n, int i, int right) {
+    int pivot = n[i][0];
+    int pivot_y = n[i][1];
+    int pivot_idx = i;
+    int j = i + 1;
+
+    while (j <= right) {
+        if (pivot > n[j][0]) {
+            i++;
+            if (i != j) Swap(n, i, j);
+        }
+        else if (pivot == n[j][0] && pivot_y > n[j][1]) {
+            i++;
+            if (i != j)Swap(n, i, j);
+        }
+        j++;
+    }
+    Swap(n, pivot_idx, i);
+    return i;
+}
+
+void Swap(int** n, int left, int right) {
+    int* temp;
+    temp = n[left];
+    n[left] = n[right];
+    n[right] = temp;
 }
