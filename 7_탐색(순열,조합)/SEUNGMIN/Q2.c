4f0b25c210cb149485ffca8a1e6374e295b71d3a
SoNgSeUNgMiN96 <87978544+SoNgSeUNgMiN96@users.noreply.github.com>
2021-09-29 00:19:39 +0900

    Update Q2.c
    프로그래머스 두 개 뽑아서 더하기  - 조합을 이용한 풀이

--- 7_탐색(순열,조합)/SEUNGMIN/Q2.c
+++ 7_탐색(순열,조합)/SEUNGMIN/Q2.c
@@ -1,1 +1,47 @@
-// 작성전
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+// numbers_len은 배열 numbers의 길이입니다.
+int* solution(int numbers[], size_t numbers_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int maxsize = numbers_len*(numbers_len-1)/2;
+    int *temp = (int*)malloc(sizeof(int)*maxsize);
+    int index=0;
+    int tempnum;
+    
+    
+    for (int i=0;i<numbers_len-1;i++){  //우선 다 넣기.. 이후         중복제거와 정렬을 해야함
+        for(int j=i+1;j<numbers_len;j++){
+            temp[index] = numbers[i]+numbers[j];
+            index++;
+        }
+    }
+    
+    for (int i=maxsize-1;i>0;i--){      //버블정렬
+        for (int j=0;j<i;j++){
+            if(temp[j]>temp[j+1]){
+                tempnum = temp[j+1];
+                temp[j+1] = temp[j];
+                temp[j] = tempnum;
+            }
+        }
+    }
+    
+    int *answer = (int*)malloc(sizeof(int)*maxsize);
+
+    int same =-1; 
+    int j=0;
+    
+    for (int i=0;i<maxsize;i++){
+        if (same !=temp[i]){
+            answer[j] = temp[i];
+            same = temp[i];
+            j++;
+        }
+    }
+    //찌꺼기 제거 필요 , 안해도 정답은됨
+    
+    return answer;
+}
